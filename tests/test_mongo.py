7a65602136ccdd039a93e394e42eeda672c83ffb
Yuan Ling <yling@splunk.com>
2021-02-25 13:12:14 -0800

    fix: clean up code

--- tests/test_mongo.py
+++ tests/test_mongo.py
@@ -6,7 +6,6 @@ import os
 
 def main():
     mongo_config = {'host': 'localhost', 'port': 27017, 'database': 'test', 'collection': 'mibs'}
-    # mongo_config = {'host': 'localhost', 'port': 27017, 'database': 'files', 'collection': 'mib_files'}
     mongo = MibsRepository(mongo_config)
 
     mongo.clear()
@@ -16,19 +15,18 @@ def main():
     mongo.upload_files(mib_files_dir)
 
     # Search mib files based on oid
-    # oid = "1, 3, 6, 1, 4, 1, 43, 2, 29, 1, 1"
-    # oid = "1, 3, 6, 1, 4, 1, 1978, 2, 14, 1, 1, 1"
-    # filename = mongo.search_oid(oid)
-    # print(filename)
+    oid = "1, 3, 6, 1, 4, 1, 43, 2, 29, 1, 1"
+    filename = mongo.search_oid(oid)
+    print(filename)
     
-    # # Delete one specifc file
-    # if filename:
-    #     mongo.delete_mib(filename)
-    #     print(mongo.search_oid(oid))
+    # Delete one specifc file
+    if filename:
+        mongo.delete_mib(filename)
+        print(mongo.search_oid(oid))
 
-    # # Delete multiple files
-    # filename_regex="A3COM"
-    # mongo.delete_mib(filename_regex)
+    # Delete multiple files
+    filename_regex="A3COM"
+    mongo.delete_mib(filename_regex)
 
 
 if __name__ == '__main__':
