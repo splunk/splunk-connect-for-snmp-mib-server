1207b5d1eafdcafb07fa79e1ff81dcc5d906b07e
pszkamruk-splunk <75434853+pszkamruk-splunk@users.noreply.github.com>
2021-04-21 09:15:25 +0200

    feat: added job which checks if code is formatted correctly in Circle… (#20)
    * feat: added job which checks if code is formatted correctly in CircleCi, code formatted using black
    
    * Creating file for storing CLA Signatures
    
    * @weliasz has signed the CLA from Pull Request #24
    
    * @pszkamruk-splunk has signed the CLA from Pull Request #20
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- splunk_connect_for_snmp_mib_server/mib_server.py
+++ splunk_connect_for_snmp_mib_server/mib_server.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, abort, jsonify, send_from_directory
 from flask_autoindex import AutoIndex
 import os
-import yaml 
+import yaml
 from splunk_connect_for_snmp_mib_server.mongo import MibsRepository, OidsRepository
 from splunk_connect_for_snmp_mib_server.translator import Translator
 import logging
 
 logger = logging.getLogger(__name__)
+
 
 class MibServer:
     def __init__(self, args, server_config):
@@ -14,45 +15,44 @@ class MibServer:
         self._server_config = server_config
         self._translator = Translator(server_config)
         self._flask_app = self.build_flask_app()
-    
-    def build_flask_app(self):    
+
+    def build_flask_app(self):
         app = Flask(__name__)
         # ppath = os.environ['MIBS_SERVER_URL']
         ppath = self._server_config["snmp"]["mibs"]["mibs_path"]
         files_index = AutoIndex(app, ppath, add_url_rules=False)
 
-        @app.route('/')
+        @app.route("/")
         def hello():
             return "Hello, This is SNMP MIB server!"
 
         # Custom indexing
-        @app.route('/files/')
-        @app.route('/files/<path:path>')
-        def autoindex(path='.'):
+        @app.route("/files/")
+        @app.route("/files/<path:path>")
+        def autoindex(path="."):
             logger.debug(f"path: {path}")
             return files_index.render_autoindex(path)
 
         # Translate oid
-        @app.route('/translation', methods=['POST'])
+        @app.route("/translation", methods=["POST"])
         def translator():
             logger.debug(request.json)
-            var_binds = request.json.get('var_binds')
+            var_binds = request.json.get("var_binds")
             metric = request.args.get("metric")
             logger.debug(f"type of var_binds: {type(var_binds)}")
             logger.debug(f"var_binds: {var_binds}")
             logger.debug(f"type of metric: {str(metric)} -- metric: {metric}")
             if metric == "True":
                 # TODO
-                # If metric is true, var_binds just has one 
+                # If metric is true, var_binds just has one
                 var_bind = var_binds[0]
-                result = self._translator.format_metric_data(var_bind)                
+                result = self._translator.format_metric_data(var_bind)
             else:
                 result = self._translator.format_trap_event(var_binds)
             return result
-        
+
         return app
-    
+
     def run_mib_server(self):
         # TODO poetry run fails when debug=True
-        self._flask_app.run(host="0.0.0.0",port=self._args.port)
-
+        self._flask_app.run(host="0.0.0.0", port=self._args.port)
--- tests/test_mongo.py
+++ tests/test_mongo.py
@@ -1,33 +1,39 @@
 import sys
-sys.path.append('../')
+
+sys.path.append("../")
 from splunk_connect_for_snmp_mib_server.mongo import MibsRepository
 import os
 
 
 def main():
-    mongo_config = {'host': 'localhost', 'port': 27017, 'database': 'test', 'collection': 'mibs'}
+    mongo_config = {
+        "host": "localhost",
+        "port": 27017,
+        "database": "test",
+        "collection": "mibs",
+    }
     mongo = MibsRepository(mongo_config)
 
     mongo.clear()
 
     # Upload  mib files in /mibs/test to mongo
-    mib_files_dir = os.path.join(os.getcwd(), "..", "mibs","test")
+    mib_files_dir = os.path.join(os.getcwd(), "..", "mibs", "test")
     mongo.upload_files(mib_files_dir)
 
     # Search mib files based on oid
     oid = "1, 3, 6, 1, 4, 1, 43, 2, 29, 1, 1"
     filename = mongo.search_oid(oid)
     print(filename)
-    
+
     # Delete one specifc file
     if filename:
         mongo.delete_mib(filename)
         print(mongo.search_oid(oid))
 
     # Delete multiple files
-    filename_regex="A3COM"
+    filename_regex = "A3COM"
     mongo.delete_mib(filename_regex)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
