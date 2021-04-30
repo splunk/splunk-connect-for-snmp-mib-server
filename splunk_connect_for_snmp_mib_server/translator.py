c2c9a8f6b3b51b662a94835ff45f8d0fc6239881
Addon Factory template <addonfactory@splunk.com>
2021-04-29 20:00:54 -0400

    feat(metrics): use each index of an array should be a splitby

--- splunk_connect_for_snmp_mib_server/translator.py
+++ splunk_connect_for_snmp_mib_server/translator.py
@@ -350,6 +350,10 @@ class Translator:
         custom_translated_oid = self.custom_translator(oid)
 
         # Construct metric data
+        snmp_index = translated_oid.split(".")[-1]
+        if snmp_index.isnumeric():
+            metric_data["snmp_index"] = snmp_index
+            translated_oid = translated_oid.removesuffix(f".{snmp_index}")
         metric_data[
             "metric_name"
         ] = f'sc4snmp.{translated_oid.replace(".","_").replace("::", ".")}'
