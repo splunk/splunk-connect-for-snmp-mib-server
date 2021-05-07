c988cf1631bf6a3debe7b673681552f0f1ea6117
lstoppa <lstoppa@splunk.com>
2021-05-07 14:58:01 +0200

    feat: add unit-test to MIB server (#2) - added negative tests for format_trap

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -22,17 +22,17 @@ class TranslatorTest(TestCase):
             },
         )
         cls.env_patcher.start()
-
         super().setUpClass()
 
     @mongomock.patch()
-    def test_format_trap(self):
+    def setUp(self):
         with open("../config.yaml", "r") as yamlfile:
             server_config = yaml.load(yamlfile, Loader=yaml.FullLoader)
         server_config["snmp"]["mibs"]["dir"] = "../mibs/pysnmp"
+        self.my_translator = Translator(server_config)
 
-        my_translator = Translator(server_config)
-
+    @mongomock.patch()
+    def test_format_trap(self):
         input_var_binds_list = [
             {
                 "oid": "1.3.6.1.2.1.1.3.0",
@@ -48,7 +48,44 @@ class TranslatorTest(TestCase):
             },
         ]
 
-        translated_var_binds = my_translator.format_trap_event(input_var_binds_list)
+        translated_var_binds = self.my_translator.format_trap_event(
+            input_var_binds_list
+        )
         assert len(translated_var_binds) > 0
         assert translated_var_binds.find("SNMPv2-MIB::sysUpTime.0") >= 0
         assert translated_var_binds.find("SNMPv2-MIB::snmpTrapOID.0") >= 0
+
+    @mongomock.patch()
+    def test_format_trap_non_existing_oid(self):
+        input_var_binds_list = [
+            {
+                "oid": "1.3.6.1.2.1111.111.111.111.444.555.555.555",
+                "oid_type": "ObjectName",
+                "val": "123",
+                "val_type": "TimeTicks",
+            },
+            {
+                "oid": "1.3222.6333.1444.65555.343434343.3434343.3.1.1.4.1.0",
+                "oid_type": "ObjectName",
+                "val": "1.3.6.1.4.1.1978.2.14.1.1.1",
+                "val_type": "ObjectIdentifier",
+            },
+        ]
+
+        translated_var_binds = self.my_translator.format_trap_event(
+            input_var_binds_list
+        )
+
+        untranslated = ""
+        for i in range(0, len(input_var_binds_list)):
+            oid_type = input_var_binds_list[i]["oid_type"]
+            value_type = input_var_binds_list[i]["val_type"]
+            oid = input_var_binds_list[i]["oid"]
+            value = input_var_binds_list[i]["val"]
+            current = f'oid-type{i+1}="{oid_type}" value{i+1}-type="{value_type}" {oid}="{value}" value{i+1}="{value}"'
+            # these two additional spaces are not an error
+            untranslated += f"{current}  "
+            if i < len(input_var_binds_list) - 1:
+                untranslated += "\n"
+
+        assert untranslated == translated_var_binds
