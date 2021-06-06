b40e7cb08b4f6eb03abb68a70f4612be0c6cf02c
rfaircloth-splunk <rfaircloth@splunk.com>
2021-06-06 14:07:24 -0400

    test: fix path to config

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -78,7 +78,7 @@ class TranslatorTest(TestCase):
 
     @mongomock.patch()
     def setUp(self):
-        with open("../config.yaml", "r") as yamlfile:
+        with open("./config.yaml", "r") as yamlfile:
             server_config = yaml.load(yamlfile, Loader=yaml.FullLoader)
         server_config["snmp"]["mibs"]["dir"] = "../mibs/pysnmp"
         self.my_translator = Translator(server_config)
